5fa9a3ca088cb9813616916c5116552f263a6024
tracer-ics2022 <tracer@njuics.org>
2022-12-22 23:15:23 +0800

    >  compile NEMU 201220129 jiangmingxu Linux mxj 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:15:23 up  3:45,  1 user,  load average: 3.12, 3.14, 3.08

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -56,10 +56,10 @@ enum
   {                                                          \
     *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); \
   } while (0)
-#define immJ()                                                                                                \
-  do                                                                                                          \
-  {                                                                                                           \
-    *imm = SEXT(BITS(i, 31, 31) << 19 | BITS(i, 19, 12) << 11 | BITS(i, 20, 20) << 10 | BITS(i, 30, 21), 20); \
+#define immJ()                                                                                                       \
+  do                                                                                                                 \
+  {                                                                                                                  \
+    *imm = SEXT((BITS(i, 31, 31) << 19 | BITS(i, 19, 12) << 11 | BITS(i, 20, 20) << 10 | BITS(i, 30, 21)) << 1, 20); \
   } while (0)
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type)
@@ -119,7 +119,7 @@ static int decode_exec(Decode *s)
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I, R(dest) = src1 + imm);
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc, U, R(dest) = imm + s->pc);
 
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, R(dest) = s->pc + 4, s->dnpc = s->dnpc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, R(dest) = s->dnpc, s->dnpc = s->dnpc + imm - 4);
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
   INSTPAT_END();
