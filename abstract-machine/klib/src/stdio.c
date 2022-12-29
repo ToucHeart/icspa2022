b0ca3be8b1f266084b5ba5e8dc27cd90d91aa564
tracer-ics2022 <tracer@njuics.org>
2022-12-29 18:41:02 +0800

    >  compile NEMU 201220129 jiangmingxu Linux mxj 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:41:02 up 11:44,  1 user,  load average: 1.26, 1.37, 1.35

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,26 +2,72 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+int printf(const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char *out, const char *fmt, ...)
+{
+  int count = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int num;
+  char *cp = NULL;
+  while (*fmt != '\0')
+  {
+    char c = *fmt++;
+    if (c == '%')
+    {
+      switch (*fmt)
+      {
+      case 'd':
+        num = va_arg(ap, int);
+        char tmp[20];
+        int len = 0;
+        while (num != 0)
+        {
+          tmp[len++] = num % 10 + '0';
+          num /= 10;
+        }
+        --len;
+        while (len >= 0)
+        {
+          out[count++] = tmp[len--];
+        }
+        break;
+      case 's':
+        cp = va_arg(ap, char *);
+        memcpy(out + count, cp, strlen(cp));
+        count+=strlen(cp);
+        break;
+      default:;
+        break;
+      }
+      fmt++;
+    }
+    else
+      out[count++] = c;
+  }
+  return count;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
