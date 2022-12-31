30dec4d4b1c81224d19e608d55442399f5ac9a1e
tracer-ics2022 <tracer@njuics.org>
2022-12-31 14:41:45 +0800

    >  compile NEMU 201220129 jiangmingxu Linux mxj 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:41:45 up  1:43,  1 user,  load average: 1.31, 0.92, 1.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -183,6 +183,12 @@ void cpu_exec(uint64_t n)
     Log("nemu: %s at pc = " FMT_WORD,
         (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) : (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
         nemu_state.halt_pc);
+#ifdef CONFIG_ITRACE_COND
+    if (nemu_state.halt_ret)
+    {
+      print_instr_ringbuf(1);
+    }
+#endif
     // fall through
   case NEMU_QUIT:
     statistic();
